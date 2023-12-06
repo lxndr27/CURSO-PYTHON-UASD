5a71cbe1c7ad900601a43f9a78495971db306ee6
Alexander García <121246622+lxndr27@users.noreply.github.com>
2023-12-06 12:08:08 -0400

    Delete UASD curso python/Practica 2 directory

--- UASD curso python/Practica 2/ejercicio_3.py
+++
@@ -1,8 +0,0 @@
-#Hacer un programa que genere las tablas de multiplicar de los números múltiplos de 5 que hay entre 1 y 1000
-for num in range(5, 1001, 5):
-    print(f"Tabla de multiplicar de {num}:")
-    for i in range(1, 11):
-        result = num * i
-        print(f"{num} x {i} = {result}")
-    print("\n")
-
